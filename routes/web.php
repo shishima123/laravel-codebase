ea56879c0b0a623fe6d3310acdaaa6e5205ee085
Phuoc Nguyen <shishima21@gmail.com>
2023-03-27 17:50:41 +0700

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Facades\StorageHelper;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/upload', function() {
+//    $file = public_path('dummy/img_category/accessory.jpeg');
+
+    $file = public_path('google.pdf');
+    $rs = StorageHelper::copy('abc.pdf', '/');
+//
+//    $path ='google_64214b95887ef.pdf';
+//    $rs =  StorageHelper::delete($path);
+        dd($rs);
+});
+
+Route::post('/upload', function(Request $request) {
+    $rs = StorageHelper::store($request->file);
+    dd($rs);
+});
